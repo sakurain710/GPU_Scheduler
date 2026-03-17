cd20afeb9cb36664c110b4c4e86f3555d029d67d
MinatoAqua114514 <2314445395@qq.com>
2026-03-17 14:24:34 +0800

    创建并初始化项目，添加依赖pom

---
+++ src/main/java/com/sakurain/gpuscheduler/GpuSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.sakurain.gpuscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GpuSchedulerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GpuSchedulerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sakurain/gpuscheduler/GpuSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sakurain.gpuscheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GpuSchedulerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
